2ab6e66a3afd2a4663de553694c8c0550f6d2850
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-04-19 19:23:53 +0900

    test: test 코드 추가

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,7 +1,29 @@
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('', () {
+  final db = FakeFirebaseFirestore();
 
+  setUp(() async {
+    await db
+        .collection('test')
+        .doc('id01')
+        .set({'title': 'title', 'body': 'body'});
+  });
+
+  group('firestore', () {
+    test('get()', () async {
+      final result = await db.collection('test').doc('id01').get();
+      expect(result.data()!['title'], equals('title'));
+      expect(result.data()!['body'], equals('body'));
+    });
+
+    test('snapshot()', () async {
+      final result = await db.collection('test').snapshots().first;
+
+      expect(result.docs.length, equals(1));
+      expect(result.docs.first.data()['title'], equals('title'));
+      expect(result.docs.first.data()['body'], equals('body'));
+    });
   });
 }
